9ae8b520504986b48abe8ca6210a7d38be424875
charles <1592276395@qq.com>
2024-09-27 22:21:40 +0800

    adjust the code format and style by clangd.

--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 if [ -d "build" ]; then
-    rm -rf build
+  rm -rf build
 fi
 
 if [ -d "bin" ]; then
-    rm -rf bin
+  rm -rf bin
 fi
 
 mkdir bin
 
 mkdir build && cd build
-cmake -G "MinGW Makefiles" ..
+
+# cmake -G "MinGW Makefiles" ..
+if [ "$(uname)" == "Linux" ]; then
+  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+else
+  cmake -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+fi
+
 make
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,71 +1,69 @@
 #include "menu.h"
+
 #include "skiplist.h"
 
-int main()
-{
-    Skiplist<int, std::string> skiplist(16);
-    skiplist.SetStressTest(0);
-    while (1)
-    {
-        int choice = MenuChoose();
-        switch (choice)
-        {
-        case 0:
-            std::cout << "See you next time~" << std::endl;
-            return 0;
-            break;
+int main() {
+  Skiplist<int, std::string> skiplist(16);
+  skiplist.SetStressTest(0);
+  while (1) {
+    int choice = MenuChoose();
+    switch (choice) {
+      case 0:
+        std::cout << "See you next time~" << std::endl;
+        return 0;
+        break;
 
-        case 1:
-            Insert(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
+      case 1:
+        Insert(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
 
-        case 2:
-            Delete(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
+      case 2:
+        Delete(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
 
-        case 3:
-            Search(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
+      case 3:
+        Search(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
 
-        case 4:
-            Display(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
+      case 4:
+        Display(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
 
-        case 5:
-            InsertSet(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
+      case 5:
+        InsertSet(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
 
-        case 6:
-            DumpFile(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
+      case 6:
+        DumpFile(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
 
-        case 7:
-            LoadFile(skiplist);
-            std::cout << "*****************************" << std::endl;
-            system("pause");
-            system("cls");
-            break;
-        }
+      case 7:
+        LoadFile(skiplist);
+        std::cout << "*****************************" << std::endl;
+        system("pause");
+        system("cls");
+        break;
     }
+  }
 
-    std::cout << "Test over, exiting..." << std::endl;
-    return 0;
+  std::cout << "Test over, exiting..." << std::endl;
+  return 0;
 }
